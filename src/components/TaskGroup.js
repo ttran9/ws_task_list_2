e625d4b54552af6e58f44318202804467a9f8664
todd <toddtran9@gmail.com>
2018-05-26 00:29:06 -0700

    Modified how TaskGroup names are displayed.

--- src/components/TaskGroup.js
+++ src/components/TaskGroup.js
@@ -1,74 +1,30 @@
 import React, { Component } from "react";
-import "../stylesheets/TaskGroup.css";
-import "../stylesheets/ListItem.css";
-import Task from "./Task";
-import groupImage from "../images/Group.svg";
 
 /**
- * This component represents each individual TaskGroup that holds individual tasks which are grouped/separated by
- * the "group" property from the original payload sample data.
+ * This component displays a list of the group names and each group name (list item) has an event handler registered
+ * to it that allows for displaying tasks inside of the selected group.
  */
 class TaskGroup extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isShowing: false,
-      groupNumber: this.props.groupNumber,
-      numberOfTasks: this.props.taskGroup.length,
-      completedTasks: this.props.completedTasks
-    }
-  }
-
-  /**
-   * This is an event handler which is responsible for handling when the TaskGroup is clicked on and toggles between
-   * displaying the list of tasks inside of a group or hiding them.
-   */
-  hideGroup() {
-    let isShowing = this.state.isShowing;
-    isShowing = !isShowing;
-    this.setState({
-      isShowing: isShowing
-    });
-  }
-
   render() {
-    let displayType = this.state.isShowing? '' : 'hideGroup';
-    let completedTaskSpanId = "completedTasks " + this.state.groupNumber;
     /**
-     * takes in an array with the groups and then prints them in separate unordered list groups.
+     * this just takes in an array of group names and displays them and uses
+     * an on click event handler to modify the parent container to display a different component
+     * which displays the tasks inside of the group.
      */
     return (
-        <div>
-          <div className="TaskGroup">
-            <div className="TaskGroupButton">
-              <input type="image"
-                     src={groupImage}
-                     alt="Group"
-                     onClick={() => this.hideGroup()}/>
-            </div>
-            <div className="TaskGroupContent">
-              <div>
-                Task Group {this.state.groupNumber}
-              </div>
-              <div>
-                <span id={completedTaskSpanId}>{this.state.completedTasks}</span> OF {this.state.numberOfTasks} TASKS COMPLETE
-                <ul className={displayType} id={this.props.listId}>
-                  {this.props.taskGroup.map((task, index) => {
-                    return (
-                        <Task
-                            key={index}
-                            toggleTaskCompletionHandler={this.props.toggleTaskCompletionHandler}
-                            task={task}/>
-                    )
-                  })}
-                </ul>
-              </div>
-            </div>
-          </div>
-          <hr/>
+        <div className="TaskGroupList">
+          <ul>
+            {this.props.groupNames.map((group, index) => {
+              return (
+                <li
+                  key={index}
+                  onClick={() => {this.props.getTaskListFromIndex(index)}}>
+                  {group}
+                </li>
+              );
+            })}
+          </ul>
         </div>
-
     );
   }
 }
