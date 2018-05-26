954c9b84b0f04e1177c604b94b28ff1bc38b5f15
todd <toddtran9@gmail.com>
2018-05-26 00:27:57 -0700

    creating helper functions to modify test data and modified how components are rendered.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "../stylesheets/App.css";
-import ThingsToDo from "./ThingsToDo";
-import TaskGroupList from "./TaskGroupList";
+import TaskGroupContainer from "./TaskGroupContainer";
 
 /**
  * Entry point to the task list.
@@ -11,73 +10,96 @@ class App extends Component {
     super(props);
 
     this.state = {
-      // the example payload to be displayed (add bootstrap data).
-      payload: [
-        {
-          id: 1,
-          group: "Purchases",
-          task: "Go to the bank",
-          dependencyIds: [],
-          completedAt: null,
-        },
-        {
-          id: 2,
-          group: "Purchases",
-          task: "Buy hammer",
-          dependencyIds: [1],
-          completedAt: null,
-        },
-        {
-          id: 3,
-          group: "Purchases",
-          task: "Buy wood",
-          dependencyIds: [1],
-          completedAt: null,
-        },
-        {
-          id: 4,
-          group: "Purchases",
-          task: "Buy nails",
-          dependencyIds: [1],
-          completedAt: null,
-        },
-        {
-          id: 5,
-          group: "Purchases",
-          task: "Buy paint",
-          dependencyIds: [1],
-          completedAt: null,
-        },
-        {
-          id: 6,
-          group: "Build Airplane",
-          task: "Hammer nails into wood",
-          dependencyIds: [2, 3, 4],
-          completedAt: null,
-        },
-        {
-          id: 7,
-          group: "Build Airplane",
-          task: "Paint wings",
-          dependencyIds: [5, 6],
-          completedAt: null,
-        },
-        {
-          id: 8,
-          group: "Build Airplane",
-          task: "Have a snack",
-          dependencyIds: [11],
-          completedAt: null,
-        }
-      ]
+      payload: []
     };
   }
+
+  /**
+   * the example payload to be displayed (add bootstrap data).
+   */
+  generateSamplePayload() {
+    let payload = [
+      {
+        id: 1,
+        group: "Purchases",
+        task: "Go to the bank",
+        dependencyIds: [],
+        completedAt: null,
+      },
+      {
+        id: 2,
+        group: "Purchases",
+        task: "Buy hammer",
+        dependencyIds: [1],
+        completedAt: null,
+      },
+      {
+        id: 3,
+        group: "Purchases",
+        task: "Buy wood",
+        dependencyIds: [1],
+        completedAt: null,
+      },
+      {
+        id: 4,
+        group: "Purchases",
+        task: "Buy nails",
+        dependencyIds: [1],
+        completedAt: null,
+      },
+      {
+        id: 5,
+        group: "Purchases",
+        task: "Buy paint",
+        dependencyIds: [1],
+        completedAt: null,
+      },
+      {
+        id: 6,
+        group: "Build Airplane",
+        task: "Hammer nails into wood",
+        dependencyIds: [2, 3, 4],
+        completedAt: null,
+      },
+      {
+        id: 7,
+        group: "Build Airplane",
+        task: "Paint wings",
+        dependencyIds: [5, 6],
+        completedAt: null,
+      },
+      {
+        id: 8,
+        group: "Build Airplane",
+        task: "Have a snack",
+        dependencyIds: [11],
+        completedAt: null,
+      }
+    ];
+    return payload;
+  }
+
+  /**
+   * this is a function to setting sample hard-coded payload for testing.
+   */
+  componentWillMount() {
+    let payload = this.generateSamplePayload();
+    this.setPayload(payload);
+  }
+
+  setPayload(payload) {
+    this.setState({
+      payload: payload
+    })
+  }
+
   render() {
+    this.generateSamplePayload();
+    console.log(this.state.payload);
     return (
-      <div className="container">
-        <ThingsToDo tasks={this.state.payload}/>
-        <TaskGroupList/>
-      </div>
+        <div className="container">
+          <TaskGroupContainer tasks={this.state.payload}/>
+        </div>
     );
   }
 }
